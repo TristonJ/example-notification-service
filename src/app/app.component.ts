98503961ff2cbe1cb038264a0e26fec4da41e89b
TristonJ <contact@tristonjones.com>
2020-03-19 10:03:51 -0700

    Added token initialization for testing push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from 'nativescript-plugin-firebase';
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "./app.component.html"
+  selector: "ns-app",
+  templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase
+      .init({
+        showNotifications: false,
+        showNotificationsWhenInForeground: false,
+        onPushTokenReceivedCallback: token => {
+          console.log(`Token Received: ${token}`);
+        },
+        onMessageReceivedCallback: (message: any) => {
+          console.dir(message);
+        },
+      })
+      .then(() => {
+        console.log('[Firebase] Initialized');
+      })
+      .catch(error => {
+        console.error('[Firebase] Initialize', { error });
+      });
+  }
+}
